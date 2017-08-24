6f544fdce64f3d41942548afd00b9469cc7f0030
tylergarrett <tjg3ea@virginia.edu>
2017-08-23 20:51:26 -0400

    Configured SDK and added inital button

--- FarePare/FarePare/ViewController.swift
+++ FarePare/FarePare/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import UberRides
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let button = RideRequestButton()
+        button.center = view.center
+        view.addSubview(button)
     }
 
     override func didReceiveMemoryWarning() {
